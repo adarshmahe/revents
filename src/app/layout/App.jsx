25e551a4f9720aa2fe2d92aba03a7c5d518e4ed1
Adarsh Valamary <adarsh.valamary@capgemini.com>
2022-10-31 16:23:53 +0530

    End of section 4.

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,15 +1,32 @@
-import React, {useState} from 'react';
-import { Container } from 'semantic-ui-react';
-import EventDashboard from '../../features/events/eventDashboard/EventDashboard';
-import NavBar from '../../features/nav/NavBar';
+import React, { useState } from "react";
+import { Container } from "semantic-ui-react";
+import EventDashboard from "../../features/events/eventDashboard/EventDashboard";
+import NavBar from "../../features/nav/NavBar";
 
 function App() {
   const [formOpen, setFormOpen] = useState(false);
+  const [selectedEvent, setSelectedEvent] = useState(null);
+
+  function handleSelectEvent(event) {
+    setSelectedEvent(event);
+    setFormOpen(true);
+  }
+
+  function handleCreateOpen() {
+    setSelectedEvent(null);
+    setFormOpen(true);
+  }
+
   return (
     <>
-      <NavBar setFormOpen={setFormOpen} />
+      <NavBar setFormOpen={handleCreateOpen} />
       <Container className='main'>
-        <EventDashboard formOpen={formOpen} setFormOpen={setFormOpen} />
+        <EventDashboard
+          formOpen={formOpen}
+          setFormOpen={setFormOpen}
+          selectEvent={handleSelectEvent}
+          selectedEvent={selectedEvent}
+        />
       </Container>
     </>
   );
--- src/features/events/eventDashboard/EventDashboard.jsx
+++ src/features/events/eventDashboard/EventDashboard.jsx
@@ -1,22 +1,43 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import { Grid } from "semantic-ui-react";
 import EventForm from "../eventForm/EventForm";
 import EventList from "./EventList";
-import {sampleData} from "../../../app/api/sampleData";
+import { sampleData } from "../../../app/api/sampleData";
 
-export default function EventDashboard({formOpen, setFormOpen}) {
-    const [events, setEvents] = useState(sampleData);
-    
-    return (
-        <Grid>
-            <Grid.Column width={10}>
-                <EventList events={events} />
-            </Grid.Column>
-            <Grid.Column width={6}>
-                {formOpen &&
-                    <EventForm setFormOpen={setFormOpen} />
-                }
-            </Grid.Column>
-        </Grid>
-    )
+export default function EventDashboard({ formOpen, setFormOpen, selectEvent, selectedEvent }) {
+  const [events, setEvents] = useState(sampleData);
+
+  function handleCreateEvent(event) {
+    setEvents([...events, event]);
+  }
+
+  function handleUpdateEvent(updateEvent) {
+    setEvents(events.map(evt => evt.id === updateEvent.id ? updateEvent: evt));
+    selectEvent(null);
+    setFormOpen(false);
+  }
+
+  function handleDeleteEvent(eventId) {
+    setEvents(events.filter(evt => evt.id !== eventId));
+  }
+
+  return (
+    <Grid>
+      <Grid.Column width={10}>
+        <EventList events={events} selectEvent = {selectEvent} deleteEvent={handleDeleteEvent} />
+      </Grid.Column>
+      <Grid.Column width={6}>
+        {formOpen && (
+          <EventForm
+            setFormOpen={setFormOpen}
+            setEvents={setEvents}
+            createEvent={handleCreateEvent}
+            selectedEvent= {selectedEvent}
+            updateEvent= {handleUpdateEvent}
+            key = {selectedEvent ? selectedEvent.id : null}
+          />
+        )}
+      </Grid.Column>
+    </Grid>
+  );
 }
